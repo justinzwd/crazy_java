12e4b6225ea29c220911324823fd3e95ee13e75a
justinzwd <justinzwd@gmail.com>
2020-05-26 00:11:50 +0100

    异常链

---
+++ src/chapter10/PrintStackTraceTest.java
@@ -0,0 +1,35 @@
+package chapter10;
+
+public class PrintStackTraceTest
+{
+
+    public static void main(String[] args)
+    {
+        firstMethod();
+    }
+
+    public static void firstMethod()
+    {
+        secondMethod();
+    }
+
+    public static void secondMethod()
+    {
+        thirdMethod();
+    }
+
+    public static void thirdMethod()
+    {
+        throw new SelfException("自定义异常");
+    }
+}
+
+class SelfException extends RuntimeException
+{
+    SelfException(){}
+    SelfException(String msg)
+    {
+        super(msg);
+    }
+
+}
