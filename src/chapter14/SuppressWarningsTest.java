6fb3cedc57dce2e1be55029353ee0abfb28cac06
justinzwd <justinzwd@gmail.com>
2020-05-26 01:36:08 +0100

    Annotation

---
+++ src/chapter14/SuppressWarningsTest.java
@@ -0,0 +1,15 @@
+package chapter14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings(value = "unchecked")
+public class SuppressWarningsTest
+{
+
+    public static void main(String[] args)
+    {
+        List<String> myList = new ArrayList<>();
+    }
+
+}
