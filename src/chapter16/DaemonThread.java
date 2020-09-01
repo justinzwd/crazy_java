7f13f6547d61c07ba5c3dfbf1ef2e1bcb91ae5e3
justinzwd <justinzwd@gmail.com>
2020-09-01 17:50:39 +0100

    Join Thread and Daemon Thread

---
+++ src/chapter16/DaemonThread.java
@@ -0,0 +1,27 @@
+package chapter16;
+
+public class DaemonThread extends Thread
+{
+
+    public void run()
+    {
+        for (int i=0;i<1000;i++)
+        {
+            System.out.println(getName() + " " + i);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        DaemonThread dt = new DaemonThread();
+
+        dt.setDaemon(true);
+
+        dt.start();
+
+        for (int i=0;i<10;i++)
+        {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
